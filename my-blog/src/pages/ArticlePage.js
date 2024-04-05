07c02f437481399ff35f4c3aa71ec3debb71c3fe
Khusboo Patel <kkp5601@gmail.com>
2024-04-05 18:31:10 -0400

    Commit

--- my-blog/src/pages/ArticlePage.js
+++ my-blog/src/pages/ArticlePage.js
@@ -1,19 +1,35 @@
 import { useParams } from "react-router-dom";
 import NotFoundPage from "./NotFoundPage";
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import CommentsList from "../components/CommentsList";
 
 import articles from "./article-content";
 const ArticlePage = () => {
+    const [articleInfo, setArticleInfo] = useState({ upvotes: 0, comments: [] });
     const { articleId } = useParams();
+
+    useEffect(() => {
+        const loadArticleInfo = async () => {
+            const response = await axios.get(`/api/articles/${articleId}`);
+            const newArticleInfo = response.data;
+            setArticleInfo(newArticleInfo);
+        }
+        loadArticleInfo();
+    }, []);
+
     const article = articles.find(article => article.name === articleId);
     if (!article) {
         return <NotFoundPage />
     }
     return (
         <>
-            <h1>{article?.title}</h1>
+            <h1>{article.title}</h1>
+            <p>This article has {articleInfo.upvotes} upvote(s)</p>
             {article.content.map((paragraph, i) => (
                 <p key={i}>{paragraph}</p>
             ))}
+            <CommentsList comments={articleInfo.comments} />
         </>
     );
 }
