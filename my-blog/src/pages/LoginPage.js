1967432cede59a756b045626c1e8d7e1cf02d3ff
Khusboo Patel <kkp5601@gmail.com>
2024-04-08 14:36:53 -0400

    Update Login Page

--- my-blog/src/pages/LoginPage.js
+++ my-blog/src/pages/LoginPage.js
@@ -1,6 +1,39 @@
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
+
 const LoginPage = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
+    const navigate = useNavigate();
+
+    const logIn = async() => {
+        try {
+            await signInWithEmailAndPassword(getAuth(), email, password);
+            navigate('/articles');
+        }
+        catch (err) { 
+            setError(err.message);
+        }
+    }  
+    
     return (
-        <h1>Log In</h1>
+        <>
+            <h1>Log In</h1>
+            {error && <p className="error"></p>}
+            <input
+                placeholder="Your email address"
+                value={email}
+                onChange={e=>setEmail(e.target.value)}/>
+            <input
+                placeholder="Your password"
+                value={password}
+                onChange={e=>setPassword(e.target.value)}
+                type="password" />
+            <button onClick={logIn}>Log In </button>
+            <Link to="/create-account"> Don't have an account? Create one here</Link>
+        </>
     );
 }
 
