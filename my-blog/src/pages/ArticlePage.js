07cb96c3404449f15c9e46e9048f5c8d211b7674
Khusboo Patel <kkp5601@gmail.com>
2024-04-08 16:23:20 -0400

    custome hook

--- my-blog/src/pages/ArticlePage.js
+++ my-blog/src/pages/ArticlePage.js
@@ -5,10 +5,12 @@ import axios from 'axios';
 import CommentsList from "../components/CommentsList";
 import AddCommentForm from "../components/AddCommentForm";
 import articles from "./article-content";
+import useUser from "../hooks/useUser";
 
 const ArticlePage = () => {
     const [articleInfo, setArticleInfo] = useState({ upvotes: 0, comments: [] });
     const { articleId } = useParams();
+    const { user, isLoading } = useUser();
 
     useEffect(() => {
         const loadArticleInfo = async () => {
@@ -34,15 +36,20 @@ const ArticlePage = () => {
         <>
             <h1>{article.title}</h1>
             <div className="upvotes-section">
-                <button onClick={addUpvote}>Upvote</button>
+                {user
+                    ? <button onClick={addUpvote}>Upvote</button>
+                    : <button>Log in to upvote</button>}
+                
                 <p>This article has {articleInfo.upvotes} upvote(s)</p>
             </div>
             {article.content.map((paragraph, i) => (
                 <p key={i}>{paragraph}</p>
             ))}
-            <AddCommentForm
-                articleName={articleId}
-                onArticleUpdated={updatedArticle=>setArticleInfo(updatedArticle)} />
+            {user
+                ? < AddCommentForm
+                    articleName={articleId}
+                    onArticleUpdated={updatedArticle => setArticleInfo(updatedArticle)} />
+                : <button>Login to comment</button>}
             <CommentsList comments={articleInfo.comments} />
         </>
     );
