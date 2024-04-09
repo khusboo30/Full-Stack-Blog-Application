1740215e20ece2800561bf95b98242678a078b3c
Khusboo Patel <kkp5601@gmail.com>
2024-04-09 15:47:58 -0400

    Commit

--- my-blog-backend/src/server.js
+++ my-blog-backend/src/server.js
@@ -1,13 +1,42 @@
 import express from 'express';
 import { db, connecttoDb } from './db.js';
+import fs from 'fs';
+import admin from 'firebase/admin';
+
+const credentials = JSON.parse(
+    fs.readFileSync('../credentials.json')
+);
+// Initialize Firebase Admin SDK with the credentials file
+admin.intializeApp({
+    credential: admin.credential.cert(credentials),
+});
 
 const app = express();
 app.use(express.json());
 
+//Middleware for checking if user is authenticated (has a valid token)
+app.use(async (req, res, next) => {
+    const { authtoken } = req.headers;
+    if (authtoken) {
+        try {
+            req.user = await admin.auth().verifyIdToken(authtoken);
+        }
+        catch (e) {
+            res.sendStatus(400);
+        }
+    }    
+    next();
+});
+
 app.get('/api/articles/:name', async (req, res) => {
     const { name } = req.params;
+    const { uid } = req.user;
+
     const article = await db.collection('articles').findOne({ name });
     if (article) {
+        const upvotesIds = article.upvoteIds || [];
+        // Check if current user has already up votes
+        article.canUpvote = uid && !upvotesIds.includes(uid);
         res.json(article); 
     }
     else {
@@ -24,12 +53,28 @@ app.get('/hello/:name', (req, res) => {
     res.send(`Hello ${name}!!`);
 })*/
 
+app.use((req, res, next) => {
+    if (req.user) {
+        next();
+    }
+    else {
+        res.sendStatus(401);
+    }
+});
+
 app.put('/api/articles/:name/upvote', async(req, res) => {
     const { name } = req.params;
-    await db.collection('articles').updateOne({ name }, {
-        $inc: { upvotes: 1 },  //increments the upvote count by 1 for the article with the same name. $inc is one of the mongodb operators.
-    });
     const article = await db.collection('articles').findOne({ name });
+    if (article) {
+        const upvotesIds = article.upvoteIds || [];
+        // Check if current user has already up votes
+        const canUpvote = uid && !upvotesIds.includes(uid);
+        if (canUpvote) {
+            await db.collection('articles').updateOne({ name }, {
+                $inc: { upvotes: 1 },  //increments the upvote count by 1 for the article with the same name. $inc is one of the mongodb operators.
+            });
+        }
+    }
 
     if (article) {
         res.json(article);
